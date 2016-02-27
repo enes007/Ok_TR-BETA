199abfd16edf5e2803dc594dc63a64a94497f262
Enes007 <ened1995@gmail.com>
2016-02-27 16:16:43 +0200

    Added files via upload

--- src/ok/ok_tr/merkez.php
+++ src/ok/ok_tr/merkez.php
@@ -1,1 +1,26 @@
+<?php
+//Okçuluk Yarışması Yapımcı By Enes Özdemir
+//Tamamı Kendimce Kodlanmıştır...
+namespace ok\ok_tr;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\Listener;
+class merkez extends PluginBase implements Listener{
+// Aktif Olduğunda Yapılıcaklar
+ public function onEnable(){
+ $this->getLogger()->info("√ "."[Okçu_TR] Eklenti Aktif Durumda."); 
+ $this->getServer()->getPluginManager()->registerEvents($this, $this); 
+ } 
+ //Devre Dışı Kaldığında Yapılıcaklar
+ public function onDisable(){
+ $this->getLogger()->info("× "."[Okçu_TR] Eklenti Devre Dışı."); 
+ }
 
+
+
+
+
+
+
+
+}
