1de941ebe8e56e933ff848e407fbdc79d96a2cd6
Enes007 <ened1995@gmail.com>
2016-02-27 16:53:37 +0200

    Update merkez.php

--- src/ok/ok_tr/merkez.php
+++ src/ok/ok_tr/merkez.php
@@ -8,12 +8,12 @@ use pocketmine\event\Listener;
 class merkez extends PluginBase implements Listener{
 // Aktif Olduğunda Yapılıcaklar
  public function onEnable(){
- $this->getLogger()->info("√ "."[Okçu_TR] Eklenti Aktif Durumda."); 
+ $this->getLogger()->info("√ "."«» Eklenti Aktif Durumda."); 
  $this->getServer()->getPluginManager()->registerEvents($this, $this); 
  } 
  //Devre Dışı Kaldığında Yapılıcaklar
  public function onDisable(){
- $this->getLogger()->info("× "."[Okçu_TR] Eklenti Devre Dışı."); 
+ $this->getLogger()->info("× "."«» Eklenti Devre Dışı."); 
  } 
  
 }
